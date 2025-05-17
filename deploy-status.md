8ed07ded1cea31508fe7ad4794bf9d831bf165df
alfarocanalesea <alf.can.es.al@gmail.com>
2025-05-17 13:24:11 -0600

    fix: :rocket: Fix problem

--- deploy-status.md
+++ deploy-status.md
@@ -1,1 +1,1 @@
-Deploy
+Deploy control version
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import NavbarGlobal from "@/components/constants/NavbarGlobal/NavbarGlobal";
 import BentoContainer from "@/components/sections/BentoContainer/BentoContainer";
-import Typewriter from "@/components/sections/home/Typewriter/Typewriter";
 
 export default function Home() {
   return (

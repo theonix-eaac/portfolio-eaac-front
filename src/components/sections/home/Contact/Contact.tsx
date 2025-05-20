d9cccd001dafde1ffe74d70b889ee0143b1ede50
alfarocanalesea <alf.can.es.al@gmail.com>
2025-05-20 07:37:03 -0600

    style: :lipstick: Update Contact styles and component

--- src/components/sections/home/Contact/Contact.tsx
+++ src/components/sections/home/Contact/Contact.tsx
@@ -1,13 +1,30 @@
 import { HiMail } from "react-icons/hi";
-
 import styles from "./Contact.module.css";
+import Link from "next/link";
+
+const ProtectedEmail = () => {
+  const email = "alf.can.es.al";
+  const domain = "gmail.com";
+
+  return (
+    <Link
+      href={`mailto:${email}@${domain}`}
+      aria-label="Enviar correo"
+      className={styles.mailLink}
+    >
+      <span className={styles.mailIcon}>
+        <HiMail />
+      </span>
+    </Link>
+  );
+};
 
 const Contact = () => {
   return (
     <div className={styles.container}>
       <div className={styles.title}>Contacto</div>
       <div className={styles.icon}>
-        <HiMail />
+        <ProtectedEmail />
       </div>
     </div>
   );

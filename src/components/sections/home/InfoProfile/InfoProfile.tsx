5a9df139899d4ed83553b394281a08c6eccafbbf
alfarocanalesea <alf.can.es.al@gmail.com>
2025-05-19 18:07:21 -0600

    style: :lipstick: Update styles to InfoProfile

--- src/components/sections/home/InfoProfile/InfoProfile.tsx
+++ src/components/sections/home/InfoProfile/InfoProfile.tsx
@@ -1,21 +1,21 @@
-import Link from "next/link"
-import styles from "./InfoProfile.module.css"
+import Link from "next/link";
+import styles from "./InfoProfile.module.css";
 
-const InfoProfile = ()=>{
-    return(
-        <div className={styles.info}>
-          <h1 className={styles.name}>Esaul Alejandro Alfaro Canales</h1>
-          <p className={styles.role}>Ingeniero en Nanotecnología</p>
-          <Link
-            href="https://www.cedulaprofesional.sep.gob.mx/cedula/presidencia/indexAvanzada.action"
-            rel="noreferrer"
-            target="_blank"
-            className={styles.ced}
-          >
-            Cédula: 14501319
-          </Link>
-        </div>
-    )
-}
+const InfoProfile = () => {
+  return (
+    <div className={styles.info}>
+      <h1 className={styles.name}>Esaul Alejandro Alfaro Canales</h1>
+      <p className={styles.role}>Ingeniero en Nanotecnología</p>
+      <Link
+        href="https://www.cedulaprofesional.sep.gob.mx/cedula/presidencia/indexAvanzada.action"
+        rel="noreferrer"
+        target="_blank"
+        className={styles.ced}
+      >
+        ☞  Cédula: 14501319  ☜
+      </Link>
+    </div>
+  );
+};
 
-export default InfoProfile
+export default InfoProfile;
